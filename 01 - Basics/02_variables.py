5d3ab9bcdf22c00edd74f8ea1461062c1a12846f
Viljamas Grumadas <viljamasgr@gmail.com>
2025-05-06 10:23:16 +0100

    basic folder structure
    created a basic folder structure and added first 2 lessons

---
+++ 01 - Basics/02_variables.py
@@ -0,0 +1,42 @@
+# Variable = A container for a value (string, integer, float, boolean)
+#            A variable behaves as if it was the value it contains
+
+# String variables (stores text values)
+first_name = "John"
+food = "pizza"
+email = "John123@fake.com"
+
+# Print statements to demonstrate how strings work
+print("Hello, my name is " + first_name)
+print("I love eating " + food)
+print("My email address is " + email)
+
+# Integer variables (stores whole numbers)
+age = 25
+quantity = 3
+num_of_students = 10
+
+# Print statements to demonstrate how integers work
+print("I am", age, "years old")
+print("I ordered", quantity, "pizzas")
+print("There are", num_of_students, "students in the class")
+
+# Float variables (stores decimal numbers)
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Print statements to demonstrate how floats work
+print("The price of the meal was $", price)
+print("My GPA is", gpa)
+print("The distance to my house is", distance, "km")
+
+# Boolean variables (stores True or False values)
+is_student = True
+for_sale = False
+is_online = True
+
+# Print statements to demonstrate how booleans work
+print("Am I a student?", is_student)
+print("Is this item for sale?", for_sale)
+print("Is the website online?", is_online)
