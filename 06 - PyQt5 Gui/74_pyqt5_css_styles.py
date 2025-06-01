0fdec08c88f257b1a2ed7623b26adebd133ec487
Viljamas Grumadas <viljamasgr@gmail.com>
2025-06-01 11:50:46 +0100

    Program/Lesson
    Completed a lesson on styling and made a program - digital alarm clock. The program used a custom font - ds-digit to make the font look digital.

--- 06 - PyQt5 Gui/74_pyqt5_css_styles.py
+++ 06 - PyQt5 Gui/74_pyqt5_css_styles.py
@@ -1,17 +1,52 @@
 # PyQt5 setStyleSheet()
 
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QWidget, QHBoxLayout
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setGeometry(700, 300, 500, 500)
+        self.button1 = QPushButton("#1")
+        self.button2 = QPushButton("#2")
+        self.button3 = QPushButton("#3")
         self.initUI()
         
 
     def initUI(self):
-        pass
+        central_widget = QWidget()
+        self.setCentralWidget(central_widget)
+
+        hbox = QHBoxLayout()
+
+        hbox.addWidget(self.button1)
+        hbox.addWidget(self.button2)
+        hbox.addWidget(self.button3)
+
+        central_widget.setLayout(hbox)
+
+        self.button1.setObjectName("button1")
+        self.button2.setObjectName("button2")
+        self.button3.setObjectName("button3")
+
+        self.setStyleSheet("""
+            QPushButton{
+                font-size: 40px;
+                font-family: Arial;
+                padding: 15px 75px;
+                margin: 25px;
+                border: 3px solid;
+                border-radius: 15px;
+            }
+            QPushButton#button1{
+                background-color: green;
+            }
+            QPushButton#button2{
+                background-color: orange;
+            }
+            QPushButton#button3{
+                background-color: red;
+            }
+        """)
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
